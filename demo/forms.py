804b7c39fa13c7ce8417902bd1f73b0400391d44
srikanthpragada <srikanthpragada@gmail.com>
2019-01-02 20:58:14 +0530

    2-JAN

---
+++ demo/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+
+class ContactForm(forms.Form):
+    subject = forms.CharField(max_length=100)
+    message = forms.CharField(widget=forms.Textarea)
+    sender = forms.EmailField()
+    cc_myself = forms.BooleanField(required=False)
+
+
+class AddJobForm(forms.Form):
+    title = forms.CharField(max_length=30)
+    minsal = forms.IntegerField( label='Min Salary')
+    maxsal = forms.IntegerField( label= 'Max Salary')
--- demo/job_views.py
+++ demo/job_views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import  AddJobForm
 import sqlite3
 
 
@@ -13,4 +14,29 @@ def list(request):
 
 
 def add(request):
-    return render(request, 'add_job.html')
+    if request.method == "GET":
+        f = AddJobForm()
+        return render(request, 'add_job.html',{'form' : f})
+    else:
+        f = AddJobForm(request.POST)
+        if f.is_valid():
+            title = f.cleaned_data['title']
+            minsal = f.cleaned_data['minsal']
+            maxsal = f.cleaned_data['maxsal']
+            if minsal > maxsal:
+                return render(request, 'add_job.html', {'form': f,
+                                    'message': 'Min Salary must be <= Max Salary'})
+
+            con = sqlite3.connect(r"e:\classroom\python\nov22\hr.db")
+            cur = con.cursor()
+            cur.execute("insert into jobs(title,minsal,maxsal) values(?,?,?)",
+                        (title,minsal,maxsal))
+            con.commit()
+            con.close()
+            if cur.rowcount == 1:
+                return redirect('/demo/listjobs')
+            else:
+                return render(request, 'add_job.html', {'form': f,
+                       'message' : 'Sorry! Could not add job'})
+        else:
+            return render(request, 'add_job.html', {'form': f})
